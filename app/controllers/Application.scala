dccfc2286df2c017acaf1cb0bdac82a2c47266ed
Christian Henry <christianhenry@Christians-MacBook-Pro.local>
2014-02-24 10:10:22 -0600

    initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import play.api.mvc._
+import play.api.libs.iteratee._
+import play.api.libs.concurrent.Execution.Implicits._
+
+object Application extends Controller {
+
+	def index =  WebSocket.using[String] { request =>
+		//Concurernt.broadcast returns (Enumerator, Concurrent.Channel)
+		val (out,channel) = Concurrent.broadcast[String]
+	   
+		//log the message to stdout and send response back to client
+		val in = Iteratee.foreach[String] {
+			msg => println(msg)
+			//the channel will push to the Enumerator   
+			channel push("RESPONSE: " + msg)
+		}
+		(in,out)
+	}
+}
