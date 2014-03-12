db97394ebd010eed18ca174d5babc7c6ecee8e2c
Christian Henry <christian.henry92@gmail.com>
2014-03-12 14:56:54 -0500

    moving to intellij

--- app/controllers/mongoDB.scala
+++ app/controllers/mongoDB.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.test._
 import play.api._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
@@ -22,17 +23,33 @@ object mongoDB extends Controller with MongoController {
 
 	def rooms: JSONCollection = db.collection[JSONCollection]("rooms")
 
-	// def createRoom(roomname: String): Future[LastError] = {
-	// 	val room = Room(roomname, List[Long]())
-	//     // insert the user
-	//     rooms.insert(room)
-	// }
-
 	def createRoom(roomname: String) = Action.async {
 		val room = Room(roomname, List[Long]())
 	    // insert the user
 	    val futureResult = rooms.insert(room)
 	    futureResult.map(_ => Ok)
+	}
+
+	//def containsRoom(roomname: String) = ?
+
+	def addUser(roomname: String, username: String) = {
+
+		val modifier = Json.obj("$set" -> Json.arr(
+			"users" -> Await.result(getUserList(roomname),10 seconds)
+			))
+
+		//rooms.update(Json.obj("roomname" -> roomname), modifier).result(10 seconds)
+		rooms.update(Json.obj("roomname" -> roomname), modifier).onComplete {
+			case Failure(e) => throw e
+			case Success(_) => println("successful!")
+		}
+	}
+
+	def getUserList(roomname: String): Future[List[Long]] = {
+		val futureuserlist = rooms.find(Json.obj("roomname" -> roomname)).cursor.collect[List]().map {
+			futureroomlist => futureroomlist.head.users
+		}
+		futureuserlist
 	}
 
 }
