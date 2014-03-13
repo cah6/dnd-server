457e172766f74b77a6e94d376b2b9d4823825c59
Christian Henry <christian.henry92@gmail.com>
2014-03-12 19:45:49 -0500

    almost got reactivemongo things

--- app/controllers/mongoDB.scala
+++ app/controllers/mongoDB.scala
@@ -1,6 +1,7 @@
 package controllers
 
-import play.api.test._
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import play.api._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
@@ -15,44 +16,44 @@ import reactivemongo.core.commands._
 import play.modules.reactivemongo.MongoController
 import play.modules.reactivemongo.json.collection.JSONCollection
 
-object mongoDB extends Controller with MongoController {
+// object mongoDB extends Controller with MongoController {
 
-	import play.api.data.Form
-	import models._
-  	import models.JsonFormats._
+// 	import play.api.data.Form
+// 	import models._
+//   	import models.JsonFormats._
 
-	def rooms: JSONCollection = db.collection[JSONCollection]("rooms")
+// 	def rooms: JSONCollection = db.collection[JSONCollection]("rooms")
 
-	def createRoom(roomname: String) = Action.async {
-		val room = Room(roomname, List[Long]())
-	    // insert the user
-	    val futureResult = rooms.insert(room)
-	    futureResult.map(_ => Ok)
-	}
+// 	def createRoom(roomname: String) = Action.async {
+// 		val room = Room(roomname, List[Long]())
+// 	    // insert the user
+// 	    val futureResult = rooms.insert(room)
+// 	    futureResult.map(_ => Ok)
+// 	}
 
-	//def containsRoom(roomname: String) = ?
+// 	//def containsRoom(roomname: String) = ?
 
-	def addUser(roomname: String, username: String) = {
+// 	def addUser(roomname: String, username: String) = {
 
-		val modifier = Json.obj("$set" -> Json.arr(
-			"users" -> Await.result(getUserList(roomname),10 seconds)
-			))
+// 		val modifier = Json.obj("$set" -> Json.arr(
+// 			"users" -> "newuser"//Await.result(getUserList(roomname), 10 seconds)
+// 			))
 
-		//rooms.update(Json.obj("roomname" -> roomname), modifier).result(10 seconds)
-		rooms.update(Json.obj("roomname" -> roomname), modifier).onComplete {
-			case Failure(e) => throw e
-			case Success(_) => println("successful!")
-		}
-	}
+// 		//rooms.update(Json.obj("roomname" -> roomname), modifier).result(10 seconds)
+// 		rooms.update(Json.obj("roomname" -> roomname), modifier).onComplete {
+// 			case Failure(e) => throw e
+// 			case Success(_) => println("successful!")
+// 		}
+// 	}
 
-	def getUserList(roomname: String): Future[List[Long]] = {
-		val futureuserlist = rooms.find(Json.obj("roomname" -> roomname)).cursor.collect[List]().map {
-			futureroomlist => futureroomlist.head.users
-		}
-		futureuserlist
-	}
+// 	def getUserList(roomname: String): Future[List[Long]] = {
+// 		val futureuserlist = rooms.find(Json.obj("roomname" -> roomname)).cursor.collect[List]().map {
+// 			futureroomlist => futureroomlist.head.users
+// 		}
+// 		futureuserlist
+// 	}
 
-}
+// }
 
 
 
